1e24d9c359f694461021d6886d6fea06ec4b60b8
KaterynaZhuvak <129985868+KaterynaZhuvak@users.noreply.github.com>
2023-09-10 16:40:15 -0400

    updated

---
+++ src/js/cat-api.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+axios.defaults.headers.common['x-api-key'] =
+  'live_xNlsBNR1EL34BK6Wbea8FpCcO90ihAQy1i4beoiTYcvycLQiUZHb5UOMBDprladk';
+
+export function fetchBreeds() {
+  const apiUrl = 'https://api.thecatapi.com/v1/breeds';
+
+  return axios
+    .get(apiUrl, {
+      headers: {
+        'x-api-key': axios.defaults.headers.common['x-api-key'],
+      },
+    })
+    .then(resp => {
+      if (!resp.data || resp.data.length === 0) {
+        throw new Error('No cat found for the specified breed.');
+      }
+
+      return resp.data;
+    });
+}
+
+export function fetchCatByBreed(breedId) {
+  const apiUrl = 'https://api.thecatapi.com/v1/images/search';
+
+  return axios
+    .get(apiUrl, {
+      headers: {
+        'x-api-key': axios.defaults.headers.common['x-api-key'],
+      },
+      params: {
+        breed_ids: breedId,
+      },
+    })
+    .then(resp => {
+      if (!resp.data || resp.data.length === 0) {
+        throw new Error('No cat found for the specified breed.');
+      }
+
+      return resp.data;
+    });
+}
---
+++ src/js/index.js
@@ -0,0 +1,43 @@
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import SlimSelect from 'slim-select';
+
+const select = document.querySelector('select');
+const container = document.querySelector('.cat-info');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
+loader.classList.add('visually-hidden');
+error.classList.add('visually-hidden');
+
+fetchBreeds()
+  .then(data => {
+    const dataArr = data.map(element => ({
+      text: element.name,
+      value: element.id,
+    }));
+    new SlimSelect({
+      select: select,
+      data: dataArr,
+    });
+  })
+  .catch(() => error.classList.remove('visually-hidden'));
+
+select.addEventListener('change', event => {
+  const breedId = event.currentTarget.value;
+  fetchCatByBreed(breedId)
+    .then(data => {
+        container.classList.add('visually-hidden');
+      loader.classList.remove('visually-hidden');
+      const markup = `<div class='picture-container'>
+      <img class="picture" src="${data[0].url}" alt="${data[0].breeds[0].name}" width='400'/>
+    <h1>${data[0].breeds[0].name}</h1>
+    <p>${data[0].breeds[0].description}</p>
+    <p>Temperament: ${data[0].breeds[0].temperament}</p>
+    </div>`;
+      setTimeout(() => {
+        container.classList.remove('visually-hidden');
+        loader.classList.add('visually-hidden');
+        container.innerHTML = markup;
+      }, 2000);
+    })
+    .catch(() => error.classList.remove('visually-hidden'));
+});
